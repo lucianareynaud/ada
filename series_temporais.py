22e82a12bb01a188cfdd58623c16d4de07e9c914
lucianareynaud <reynaud.luciana@gmail.com>
2025-10-02 08:55:14 -0300

    v0

--- series_temporais.py
+++ series_temporais.py
@@ -1,2 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+
+candy = pd.read_csv('candy_production.csv')
+
+candy.head(12)
+candy.tail(12)
+candy.info()
+
+candy['observation_date'] = pd.to_datetime(candy['observation_date'])
+candy.info()
+
+candy.plot(x='observation_date', y='industrial_production' )
