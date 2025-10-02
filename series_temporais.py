16f12376e7d2dd54916b11cf400afaff49cd482b
lucianareynaud <reynaud.luciana@gmail.com>
2025-10-02 09:11:46 -0300

    v0

--- series_temporais.py
+++ series_temporais.py
@@ -11,4 +11,14 @@ candy.info()
 candy['observation_date'] = pd.to_datetime(candy['observation_date'])
 candy.info()
 
-candy.plot(x='observation_date', y='industrial_production' )
+candy.plot(x='observation_date', y='industrial_production')
+
+candy_filtered = candy[candy['observation_date'] >= '2010-01-01']
+
+candy_filtered
+
+ax = candy_filtered.plot(x='observation_date', y='industrial_production', fig_size=(12, 6))
+
+xcoords = ['2011-01-01', '2012-01-01', '2013-01-01', '2014-01-01', '2015-01-01', '2016-01-01', '2017-01-01']
+for xc in xcoords:
+    plt.axvline(x=xc, color='black', linestyle='--')
