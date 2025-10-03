32b3f334f892fe1d5ddbb334c3c85f8189a0c530
lucianareynaud <reynaud.luciana@gmail.com>
2025-10-03 10:13:41 -0300

    v0

--- relations.py
+++ relations.py
@@ -3,3 +3,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 penguins = sns.load_dataset('penguins')
+penguins.head()
+
+penguins['species'].value_count()
+penguins_numeric = penguins.drop(columns=['species', 'island', 'sex'])
+penguins.corr()
