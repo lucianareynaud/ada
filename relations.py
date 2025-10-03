677604806a2498f02407c3f6e3e975bbe0cb5137
lucianareynaud <reynaud.luciana@gmail.com>
2025-10-03 10:39:08 -0300

    v0

--- relations.py
+++ relations.py
@@ -12,3 +12,8 @@ penguins_numeric.corr()
 for col in penguins.drop(columns = ['species', 'island', 'sex']):
     sns.barplot(data=penguins, x='species', y=col, ci=90)
     plt.show()
+
+#grafico de dispersao permitira que enxerguemos como um par de variaveis influencia uma caracteristica
+sns.scatterplot(x='body_mass_g', y='flipper_length_mm', hue='species', data=penguins)
+sns.scatterplot(x='body_mass_g', y='bill_length_mm', hue='species', data=penguins)
+
